d096cb5fd1e913bcbfccaf34697f4d1ccdbe3964
Sunil Kumar Nunia <sunilnunia2000@gmail.com>
2021-10-19 17:01:59 +0530

    commit

---
+++ index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta http-equiv="x-ua-compatible" content="ie=edge" />
+  <title>Los Angeles Web Design Company | SPINX Digital Agency</title>
+  <!-- MDB icon -->
+  <link rel="icon" href="assets/img/favicon-logo.png" type="image/x-icon" />
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" />
+  <!-- Google Fonts Roboto -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" />
+  <!-- MDB -->
+  <link rel="stylesheet" href="css/mdb.min.css" />
+  <!-- External CSS -->
+  <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+  <!-- Start your project here-->
+
+  <nav class="navbar" style="background-color: #3D4040; color: white; justify-content: flex-end;">
+    <div class="" style="">
+      <span class="navbar-text" style="margin-right:26px;">
+        (213) 894-9933 | hello@spinxdigital.com
+      </span>
+    </div>
+  </nav>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><img src="assets/img/f-logo.png" height="20" alt="" loading="lazy" style="margin-top: 2px;" /></a>
+      <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+
+        <!-- Right elements -->
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item me-3 me-lg-1">
+            <a class="nav-link d-sm-flex align-items-sm-center" href="#">
+              <strong class="d-sm-block ms-1">WORK</strong>
+            </a>
+          </li>
+          <li class="nav-item me-3 me-lg-1">
+            <a class="nav-link" href="#">
+              <strong class="d-sm-block ms-1">EXPERTISE</strong>
+            </a>
+          </li>
+          <li class="nav-item dropdown me-3 me-lg-1">
+            <a class="nav-link dropdown-toggle hidden-arrow" href="#" id="navbarDropdownMenuLink" role="button" data-mdb-toggle="dropdown" aria-expanded="false">
+              <strong class="d-sm-block ms-1">PROCESS</strong>
+            </a>
+          </li>
+          <li class="nav-item me-3 me-lg-1">
+            <a class="nav-link d-sm-flex align-items-sm-center" href="#">
+              <strong class="d-sm-block ms-1">AGENCY</strong>
+            </a>
+          </li>
+          <li class="nav-item dropdown me-3 me-lg-1">
+            <a class="nav-link dropdown-toggle hidden-arrow" href="#" id="navbarDropdownMenuLink" role="button" data-mdb-toggle="dropdown" aria-expanded="false">
+              <strong class="d-sm-block ms-1">RESOURCES</strong>
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
+              <li>
+                <a class="dropdown-item" href="#">BLOG</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#">GUIDES</a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item me-3 me-lg-1">
+            <a class="nav-link d-sm-flex align-items-sm-center" href="#">
+              <strong class="d-none d-sm-block ms-1">CONTACT</strong>
+            </a>
+          </li>
+        </ul>
+        <!-- Right elements -->
+      </div>
+    </div>
+  </nav>
+
+
+  <header>
+    <!-- Intro settings -->
+    <style>
+      /* Default height for small devices */
+      #intro-example {
+        height: 400px;
+      }
+
+      /* Height for devices larger than 992px */
+      @media (min-width: 992px) {
+        #intro-example {
+          height: 700px;
+        }
+      }
+    </style>
+
+    <!-- Background image -->
+    <div id="intro-example" class="p-5 text-center bg-image" style="background-image: url('assets/img/home-banner@2x.jpg');">
+      <div class="mask" style="background-color: rgba(0, 0, 0, 0.1);">
+        <div class="d-flex justify-content-end align-items-center h-100" style="margin-right: 20px;">
+          <div class="text-white">
+            <h1 class="" style="font-size: 106px; text-align:right; line-height: 90px"><b>BIG IDEAS,<br>CREATIVE PEOPLE,<br>NEW TECHNOLOGY.</b></h1>
+          </div>
+        </div>
+      </div>
+    </div>
+    <p style="background-color: #b2d426; padding:3px; display:block;">
+    <p>
+      <!-- Background image -->
+  </header>
+
+  <section>
+    <div class="" style="">
+      <div class="row">
+        <div class="col-sm-6 col-md-5 col-lg-6" style="background-color:#b2d426;">.col-sm-6 .col-md-5 .col-lg-6</div>
+        <div class="col-sm-6 col-md-5 offset-md-2 col-lg-6 offset-lg-0" style="background-color:#b2d426;">.col-sm-6 .col-md-5 .offset-md-2 .col-lg-6 .offset-lg-0</div>
+      </div>
+    </div>
+  </section>
+
+
+  <!-- End your project here-->
+
+  <!-- MDB -->
+  <script type="text/javascript" src="js/mdb.min.js"></script>
+  <!-- Custom scripts -->
+  <script type="text/javascript"></script>
+</body>
+
+</html>
